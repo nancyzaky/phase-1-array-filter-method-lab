01d8b17bdbe4003bc4d4ef593cf5c8d5af592e02
Nancy Zaky <nancyzki@yahoo.com>
2021-06-29 16:00:25 -0700

    commit tree 3bca2c056b63c21080597683190d84cb1df5ccad

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+const findMatching = (arr, str) => {
+  return arr.filter(item => item.toLowerCase() === str.toLowerCase())
+}
+
+const fuzzyMatch = (arr, str) => {
+  const lengthStr = str.length
+  return arr.filter(item => item.slice(0,lengthStr) === str);
+}
+
+const matchName = (arr, str) => {
+  const result = arr.filter (obj => obj.name=== str)
+  return result
+}
